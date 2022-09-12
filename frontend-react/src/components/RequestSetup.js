a0646dc1504ead09f18bf443c850640d8b4724cf
miko <>
2022-09-13 01:08:34 +0200

    Full CI/CD for frontend, sed localhost

--- frontend-react/src/components/RequestSetup.js
+++ frontend-react/src/components/RequestSetup.js
@@ -1,9 +1,6 @@
-// const frontEndLink = 'http://localhost:3000'
 export const backEndLink = 'http://localhost:8000'
 
 export const headersForRequests = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
 }
-
-export const modeForRequests = 'cors'
